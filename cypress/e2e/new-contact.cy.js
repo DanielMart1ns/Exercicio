2f6f1dfc13f6909561d2d6f83ac2ede3935b05e2
DanielMart1ns <danielmmrodrigues09@gmail.com>
2024-05-28 18:21:19 -0300

    feat: unifying code

--- cypress/e2e/new-contact.cy.js
+++ cypress/e2e/new-contact.cy.js
@@ -17,4 +17,45 @@ describe("New contact", () => {
       "11 98765432"
     );
   });
+
+  //contact data change tests
+  it("Changing contact name", () => {
+    cy.get(".sc-iAEyYk > :last-child > .sc-gueYoa > .edit").click();
+    cy.get('input[type="text"]').clear();
+    cy.get('input[type="text"]').type("Name Changed");
+    cy.get(".alterar").click();
+
+    cy.get(".sc-iAEyYk > :last-child > .sc-dmqHEX > .sc-eDDNvR").should(
+      "contain",
+      "Name Changed"
+    );
+  });
+
+  it("Changing contact email", () => {
+    cy.get(".sc-iAEyYk > :last-child > .sc-gueYoa > .edit").click();
+    cy.get('input[type="email"]').clear();
+    cy.get('input[type="email"]').type("changed@email.com");
+    cy.get(".alterar").click();
+    cy.get(".sc-iAEyYk > :last-child > .sc-dmqHEX > .sc-eDDNvR").should(
+      "contain",
+      "changed@email.com"
+    );
+  });
+
+  it("Changing contact telephone number", () => {
+    cy.get(".sc-iAEyYk > :last-child > .sc-gueYoa > .edit").click();
+    cy.get('input[type="tel"]').clear();
+    cy.get('input[type="tel"]').type("00 0000-0000");
+    cy.get(".alterar").click();
+    cy.get(".sc-iAEyYk > :last-child > .sc-dmqHEX > .sc-eDDNvR").should(
+      "contain",
+      "00 0000-0000"
+    );
+  });
+
+  //Removing a contact
+  it("Removing a contact", () => {
+    cy.get(".sc-iAEyYk > :last-child > .sc-gueYoa > .delete").click();
+    cy.get(".sc-iAEyYk > :last-child").next().should("not.exist");
+  });
 });
